afb1cbaf187ba28ddd03d0ee3727a05b327a6836
Ph0qu3_111 <79283690+BananeRapeuse@users.noreply.github.com>
2024-05-14 19:18:05 +0200

    Add files via upload

---
+++ credits.h
@@ -0,0 +1,8 @@
+#ifndef credits_h
+#define credits_h
+// dictionary will be sorted alphabetically
+static NSDictionary* CREDITS = @{
+@"Ph0qu3_111" : @"https://github.com/BananeRapeuse/",
+};
+
+#endif /* credits_h */
